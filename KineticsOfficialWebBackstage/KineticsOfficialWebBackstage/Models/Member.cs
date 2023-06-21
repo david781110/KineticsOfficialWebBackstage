8268be87980166873d8d2022a7b25cb7fb8f25af
KINETICS\davidliu <davidliu@kinetics.com.tw>
2023-06-21 14:04:37 +0800

    登入畫面完成，修改完後端驗證

--- KineticsOfficialWebBackstage/KineticsOfficialWebBackstage/Models/Member.cs
+++ KineticsOfficialWebBackstage/KineticsOfficialWebBackstage/Models/Member.cs
@@ -11,11 +11,11 @@ namespace KineticsOfficialWebBackstage.Models
     {
         public int UserId { get; set; }
 
-        [Required(ErrorMessage ="帳號或密碼有誤")]
+        [Required(ErrorMessage ="帳號有誤")]
         [Display(Name ="帳號")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "帳號或密碼有誤")]
+        [Required(ErrorMessage = "密碼有誤")]
         [Display(Name = "密碼")]
         public string Password { get; set; }
         public int? Status { get; set; }
